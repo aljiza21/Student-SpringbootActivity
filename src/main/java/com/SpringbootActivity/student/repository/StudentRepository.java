b0be45944a681e3a6ea3dedcab976d839c00ceaa
Aljiza Tua <aljizatua@gmail.com>
2023-05-23 22:22:38 +0800

    Spring Boot Activity - Rest API with JPA

---
+++ src/main/java/com/SpringbootActivity/student/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.SpringbootActivity.student.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.SpringbootActivity.student.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+    
+}
